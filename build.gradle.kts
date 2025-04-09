9e201d95760309a57cefdfd3af7164cdec2e2ec7
muhmmdakmal <muhmmdakmal.69@gmail.com>
2025-04-09 22:40:47 +0700

    Setup ci.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,14 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.security.oauth.boot:spring-security-oauth2-autoconfigure")
+    implementation("org.springframework.boot:spring-boot-starter-logging")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("io.springfox:springfox-boot-starter:3.0.0")
 }
 
 tasks.withType<Test> {
