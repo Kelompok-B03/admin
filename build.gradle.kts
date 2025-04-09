61136d042153b5dfe5f300c14948b488a7c6cfeb
muhmmdakmal <muhmmdakmal.69@gmail.com>
2025-04-09 23:00:40 +0700

    Fix build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    //implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation ("org.springframework.security.oauth.boot:spring-security-oauth2-autoconfigure:2.6.8")
